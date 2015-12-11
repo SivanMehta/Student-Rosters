1367650f2c6da70f586c84d3bfb75cd742939461
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-12-11 14:41:36 -0500

    initial commit

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'facebook' : {
+        clientID      : '710852702349162', // your App ID
+        clientSecret  : 'da4f80a7201f3e161d3bde5b5805fa2f', // your App Secret
+        callbackURL   : 'http://localhost:50000/auth/facebook/callback'
+    }
+}
