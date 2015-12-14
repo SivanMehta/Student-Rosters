6426dabbda99d2db5baa2a00bc523a9a487c0112
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-12-14 15:44:26 -0500

    moved all functionality to one function to prevent user from fiddling about

--- public/js/game.js
+++ public/js/game.js
@@ -5,8 +5,8 @@ $("#startGame").click(function()
     setup_game();
 });
 
-var students;
 
+var students;
 setup_game = function()
 {
     var className = $("li a").contents()[1].data;
@@ -17,61 +17,70 @@ setup_game = function()
         success: function(data, status)
         {
             students = JSON.parse(data);
-            guessTheName(students)
+            startGame()
         }
 
     });
 }
 
-// Fisher-Yates Algorithm
-shuffle = function(arr)
+function startGame()
 {
-    var shuffled = arr.slice(0), i = arr.length, temp, index;
-    while (i--) {
-        index = Math.floor((i + 1) * Math.random());
-        temp = shuffled[index];
-        shuffled[index] = shuffled[i];
-        shuffled[i] = temp;
-    }
-
-    return shuffled
-}
-
-var correct;
-var score = 0;
-
-guessTheName = function()
-{
-    students = shuffle(students);
-    var victims = students.slice(0, 4);
-    correct = Math.round(Math.random() * 4);
-
-    var pictureRow = document.createElement("div");
-    pictureRow.class = "row";
-
-    for(var i = 0; i < victims.length; i++)
+    // Fisher-Yates Algorithm
+    shuffle = function(arr)
     {
-        var thumbnail = $.parseHTML("<div class = 'col-xs-6 col-md-3' onclick = 'guessName(" + i + ")'>" + 
-                                        "<a href = '#!' class = 'thumbnail'>" + 
-                                            "<img src = " + victims[i].facebook.photo +  " >" + 
-                                        "</a>" + 
-                                    "</div>")[0];
+        var shuffled = arr.slice(0), i = arr.length, temp, index;
+        while (i--)
+        {
+            index = Math.floor((i + 1) * Math.random());
+            temp = shuffled[index];
+            shuffled[index] = shuffled[i];
+            shuffled[i] = temp;
+        }
 
-        pictureRow.appendChild(thumbnail);
+        return shuffled
     }
 
-    var question_prompt = document.createElement("b");
-    question_prompt.textContent = "Who is " + victims[correct].facebook.name + "?";
+    var correct;
+    var score = 0;
 
-    playground.appendChild(question_prompt);
-    playground.appendChild(pictureRow);
-
-}
-
-guessName = function(index)
-{
-    if(index == correct)
+    guessTheName = function()
     {
-        score += 1;
+        students = shuffle(students);
+        var victims = students.slice(0, 4);
+        correct = Math.round(Math.random() * 4);
+
+        var pictureRow = document.createElement("div");
+        pictureRow.class = "row";
+
+        for(var i = 0; i < victims.length; i++)
+        {
+            var thumbnail = $.parseHTML("<div class = 'col-xs-6 col-md-3' onclick = 'guessName(" + i + ")'>" + 
+                                            "<a href = '#!' class = 'thumbnail'>" + 
+                                                "<img src = " + victims[i].facebook.photo +  " >" + 
+                                            "</a>" + 
+                                        "</div>")[0];
+
+            pictureRow.appendChild(thumbnail);
+        }
+
+        var question_prompt = document.createElement("b");
+        question_prompt.textContent = "Who is " + victims[correct].facebook.name + "?";
+
+        playground.appendChild(question_prompt);
+        playground.appendChild(pictureRow);
+
     }
+
+    guessName = function(index)
+    {
+        if(index == correct)
+        {
+            score += 1;
+        }
+        console.log(score);
+    }
+
+    guessTheName()
+    
 }
+
