a30040b73a04c66c64782f3089207c8a8019ff41
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-12-15 10:28:23 -0500

    dynamically created connection string

--- config/database.js
+++ config/database.js
@@ -1,3 +1,19 @@
+var connection_string;
+
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD)
+{
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
+else
+{
+    connection_string = "localhost:27017/studentRosters";
+}
+
+
 module.exports = {
-    'url' : "mongodb://" + process.env.OPENSHIFT_MONGODB_DB_HOST + ":" + process.envOPENSHIFT_MONGODB_DB_PORT + "/"
+    'url' : connection_string
 };
