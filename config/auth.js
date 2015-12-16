97d1241d7970ee455036cdbd73796dec04c911ca
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-12-16 15:30:11 -0500

    removed authorization from public github

--- config/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    'facebook' : {
-        clientID      : '710852702349162', // your App ID
-        clientSecret  : 'da4f80a7201f3e161d3bde5b5805fa2f', // your App Secret
-        callbackURL   : '/auth/facebook/callback'
-    }
-}
